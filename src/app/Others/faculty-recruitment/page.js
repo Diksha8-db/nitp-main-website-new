1d5b22338c3a63fc4181cd63c31c8d172717e062
National Insitute of Technology Patna | Web Development Cell <162422736+nitp-website@users.noreply.github.com>
2026-07-09 13:12:14 +0530

    Update page.js

--- src/app/Others/faculty-recruitment/page.js
+++ src/app/Others/faculty-recruitment/page.js
@@ -26,7 +26,27 @@ const RecruitmentProcess = () => {
             </thead>
             <tbody>
 
+  <tr className="border-b border-gray-100 hover:bg-red-50 transition-colors">
+                <td className="text-left px-6 py-4 text-gray-800">
+                  <p className="leading-relaxed">
+                  Interview Schedule for Candidates Shortlisted for Stage-III in Various Departments 
+                  </p>
+                </td>
+                <td className="text-center px-6 py-4">
+                  <div className="flex flex-col items-center gap-2">
+                    <a
+                      href="https://drive.google.com/file/d/1Zvsi9Gp2ywyYPJWfIyJqHMVjgqKVivP2/view?usp=sharing"
+                      download
+                      className="inline-flex items-center gap-2 bg-red-900 text-white px-4 py-2 rounded-md hover:bg-red-800 transition-colors text-sm font-medium w-40"
+                    >
+                      <FileText className="w-4 h-4" />
+                      Download
+                    </a>
+                  </div>
+                </td>
+              </tr>
 
+                        
        <tr className="border-b border-gray-100 hover:bg-red-50 transition-colors">
                 <td className="text-left px-6 py-4 text-gray-800">
                   <p className="leading-relaxed">
